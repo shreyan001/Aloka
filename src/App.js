c329bbb655d7577457a0ec7b5e0146d440df42d7
Shreyan001 <shreyan010@gmail.com>
2022-12-03 15:57:56 +0530

    updated with ENS, running a bit slow :/

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
-import linkAccount from './1_accounts';
+import { useState } from 'react';
+import { ethers } from 'ethers';
 
 function App() {
 
-  async function requestAccount()  {
-      
-  
+  // Properties
+  // if(
+  // const useENS = () => {
+  //   const getName = (addr).then(
+  // }
 
+  const [walletAddress, setWalletAddress] = useState("");
+
+  async function requestAccount() {
+    console.log('Requesting account...');
+
+    if(window.ethereum) {
+      console.log('detected');
+
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+      } catch (error) {
+        console.log('Error connecting...');
+      }
+
+    } else {
+      alert('Meta Mask not detected');
+    }
   }
+
+  // Create a provider to interact with a smart contract
+  async function connectWallet() {
+    if(typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={linkAccount}>Connect Wallet</button>
-    
+        <button
+        
+        onClick={connectWallet}
+        
+        >Request Account</button>
+        <h3>Wallet Address: {walletAddress}</h3>
       </header>
     </div>
   );
